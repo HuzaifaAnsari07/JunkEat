0669ecd23a5b2432eb4bd052737699a474d6ae67
Ansari Huzaifa <AnsariHuzaifa899@gmail.com>
2025-07-30 16:42:13 +0000

    hawwian.jpg file is in the public folder use it in the place of pizza se

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -146,7 +146,7 @@ export const products: Product[] = [
     description: 'A tropical delight with sweet pineapple chunks and savory ham.',
     price: 13.99,
     category: 'Pizza',
-    image: 'https://placehold.co/600x400.png',
+    image: '/hawwian.jpg',
     rating: 4.0,
   },
   {
@@ -173,7 +173,7 @@ export const products: Product[] = [
     description: 'Fun, spring-shaped fries with a seasoned coating for extra flavor.',
     price: 4.49,
     category: 'Fries',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://placehold.co/600x400.png',,
     rating: 4.5,
   },
   {
