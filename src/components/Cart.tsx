8ba0a27b517ed25e3fa6ff0bb93c1a9e1cc9758d
Ansari Huzaifa <AnsariHuzaifa899@gmail.com>
2025-08-27 10:50:15 +0000

    increase more height of id="order"

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -90,7 +90,7 @@ export function Cart() {
         </div>
       </ScrollArea>
   
-      <div className="flex-shrink-0 border-t bg-background px-6 py-6" id="order">
+      <div className="flex-shrink-0 border-t bg-background px-6 py-10" id="order">
         <div className="space-y-4">
             <div className="flex justify-between text-lg font-bold text-foreground">
               <p>Subtotal</p>
