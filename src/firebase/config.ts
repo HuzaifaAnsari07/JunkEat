f0b3d8cad3b21bbbd777f4abe85e8ff4f5815bfe
Ansari Huzaifa <AnsariHuzaifa899@gmail.com>
2025-10-30 05:03:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2341500839-a2d77",
+  "appId": "1:995716143769:web:31f1741d88896ceac37608",
+  "apiKey": "AIzaSyDVHB19oRyOUHvkgE3gYz8DIDYKZG-tCX0",
+  "authDomain": "studio-2341500839-a2d77.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "995716143769"
+};
